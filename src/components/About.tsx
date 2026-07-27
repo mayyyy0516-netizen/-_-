51e672fcc30990d4002e46a5eb4881a046cfb797
mayyyy0516-netizen <mayyyy0516@gmail.com>
2026-07-27 11:52:13 +0800

    Replace about portrait with dog photo

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,8 +13,7 @@ export default function About() {
     <SectionLabel index="01">ABOUT ME / 关于我</SectionLabel>
     <div className="about-layout">
       <div className="portrait-wrap">
-        {/* 头像来自个人作品集 PDF，可继续替换为更高分辨率原图 */}
-        <img src={`${import.meta.env.BASE_URL}assets/portfolio/portrait.webp`} alt="窦桐个人头像" />
+        <img src={`${import.meta.env.BASE_URL}assets/portfolio/portrait-dog.jpg`} alt="微笑的白色博美犬" />
         <div className="portrait-tag"><span>MAY</span><small>OPERATION DESIGNER</small></div>
       </div>
       <div className="about-copy">
